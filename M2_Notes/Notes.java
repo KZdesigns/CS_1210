905ccbbe05d04d582ec11d65e941134f304127f2
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-08-22 11:10:41 -0700

    playing the java.util.Scanner

--- M2_Notes/Notes.java
+++ M2_Notes/Notes.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Notes {
 
    public static void main(String[] args) {
@@ -12,13 +15,47 @@ public class Notes {
       
     */
     
+    /*
       int count = 0;
       
       int total = ++count;
       
       // prints total which is equal to 1
       System.out.println(total);
-   
+      
+     */
+     
+      /*
+      double change = 1.85;
+      int money = (int) change;
+      
+      System.out.println(money);
+      System.out.println(change);
+      */
+      
+      String name;
+      int age;
+      int birth_year;
+      final int CURRENT_YEAR = 2021;
+      Scanner scan = new Scanner(System.in);
+      
+      System.out.println("Please type your name and age below:");
+      name = scan.nextLine();
+      age = scan.nextInt();
+      
+      
+      birth_year = CURRENT_YEAR - age;
+      
+      
+      System.out.println("Hi " + name + " were born in " + birth_year + ".");
+      
+      
+      
+      
+      
+      
+      
+      
       
    
    }
