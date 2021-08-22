3aa48c958cb723f410c2870803e7f02ffdbe0367
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-08-21 17:39:11 -0700

    ++, and -- operators and how they effect variables in expressesion how to use

--- escaping_chars.java
+++ escaping_chars.java
@@ -1,8 +1,25 @@
 public class escaping_chars {
 
    public static void main(String[] args) {
+   /*
    
-      System.out.println("checking to see how backspace \\works");
+      int count = 0;
+      
+      int total = count++;
+      
+      // prints total which is equal to 0
+      System.out.println(total);
+      
+    */
+    
+      int count = 0;
+      
+      int total = ++count;
+      
+      // prints total which is equal to 1
+      System.out.println(total);
+   
+      
    
    }
 
