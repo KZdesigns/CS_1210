004dd7fbf84b2b8e6003536a56dc1ebb5d295429
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-08-23 13:32:12 -0700

    project_2 expression complete, and tested

--- M2_Project/Expression.java
+++ M2_Project/Expression.java
@@ -1,8 +1,40 @@
+import java.util.Scanner;
+
+/**
+    * Simple program to print a result of a calculation.
+    * Project_2(Expression)
+    * @author Kyle Zimmerman - CS 1210
+    * @version 8/23/2021
+    */
+
 public class Expression {
+
+/**
+     * Prints the result of the calculation and takes in user input.
+     * @param args Command line arguments –not used.
+     */
 
    public static void main(String[] args) {
    
-      
+      Scanner scan = new Scanner(System.in);
+      double x, y, result;
+        
+      System.out.println("result = (10x + 32.6) (5y - 1.567) / xy");
+      System.out.print("\tx = ");
+      x = scan.nextDouble();
+      System.out.print("\ty = ");
+      y = scan.nextDouble();
+        
+      if (x * y == 0) {
+        
+         System.out.println("result is \"undefined\"");
+         
+      } else {
+         
+         result = ((10 * x) + 32.6) * ((5 * y) - 1.567) / (x * y);
+         System.out.println("result = " + result);
+        
+      }
    
    }
 
